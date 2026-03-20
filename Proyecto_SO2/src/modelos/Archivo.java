e43c7bffc1243308acfcd5f84c8d24d2db215a5a
Jesuavi <jesus.delgado@correo.unimet.edu.ve>
2026-03-20 16:26:31 -0400

    implementa clase Proceso y control de concurrencia (Locks) en Archivo

--- Proyecto_SO2/src/modelos/Archivo.java
+++ Proyecto_SO2/src/modelos/Archivo.java
@@ -29,4 +29,44 @@ public class Archivo {
     public int getTamaño() { return tamañoEnBloques; }
     public String getDueño() { return dueño; }
     public ListaEnlazada<Bloque> getBloquesAsignados() { return bloquesAsignados; }
+    
+    // --- MÉTODOS DE CONCURRENCIA (SPRINT 3) ---
+
+    // 1. Un proceso pide permiso para LEER (Lock Compartido)
+    public boolean adquirirLockLectura() {
+        // Si alguien está escribiendo (modificando), no podemos leer. ¡Nos bloqueamos!
+        if (escribiendo == true) {
+            return false; // Permiso denegado
+        }
+        // Si nadie está escribiendo, ¡somos bienvenidos! (Incluso si hay otros leyendo)
+        lectoresActivos++;
+        return true; // Permiso concedido
+    }
+
+    // 2. El proceso termina de leer y suelta el archivo
+    public void liberarLockLectura() {
+        if (lectoresActivos > 0) {
+            lectoresActivos--;
+        }
+    }
+
+    // 3. Un proceso pide permiso para ESCRIBIR/MODIFICAR (Lock Exclusivo)
+    public boolean adquirirLockEscritura() {
+        // Si hay gente leyendo o alguien más está escribiendo, no podemos tocarlo. ¡Nos bloqueamos!
+        if (lectoresActivos > 0 || escribiendo == true) {
+            return false; // Permiso denegado
+        }
+        // Si está completamente libre, le ponemos el candado rojo
+        escribiendo = true;
+        return true; // Permiso concedido
+    }
+
+    // 4. El proceso termina de modificar y quita el candado rojo
+    public void liberarLockEscritura() {
+        escribiendo = false;
+    }
+    
+    // Métodos extra para saber el estado actual del archivo
+    public boolean isEscribiendo() { return escribiendo; }
+    public int getLectoresActivos() { return lectoresActivos; }
 }
